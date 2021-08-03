3fdf470e99dcbf8f1d3b9b0e1a55ee8fff2ffcd7
unknown <bfml002@gmail.com>
2021-08-03 22:05:28 +0000

    done!

--- js1.js
+++ js1.js
@@ -9,6 +9,7 @@ function populateCustomers() {
     if (window.XMLHttpRequest) {
         // Mozilla, Safari, IE7+ ...
         ajax = new XMLHttpRequest();
+        
     } else if (window.ActiveXObject) {
         // IE 6 and older
         ajax = new ActiveXObject('Microsoft.XMLHTTP');
@@ -18,29 +19,14 @@ function populateCustomers() {
         ajax.onreadystatechange = function() {
         if (ajax.readyState === 4 && ajax.status === 200) {
            // alert(ajax.responseText);
-            customerData = ajax.responseText;
-            console.log(customerData);
-           
-        }
-        };
-
-        // start the AJAX request
-        ajax.open('GET', 'http://localhost:8080/api/customer', true);
-        ajax.setRequestHeader('Content-type', 'application/json');
-        ajax.send();
-      
-        var elementStr;
+            customerData = JSON.parse(ajax.responseText);
+            var elementStr;
        
         var customersTable = document.getElementById("customer-table");
     
         var row;
-        var customerData2 = [
-            {"id":1,"firstName":"Rui","lastName":"Ferrão","email":"rui@gmail.com","phone":"777888"},
-            {"id":2,"firstName":"Sergio","lastName":"Gouveia","email":"sergio@gmail.com","phone":"777999"},
-            {"id":3,"firstName":"Bruno","lastName":"Ferreira","email":"bruno@gmail.com","phone":"777666"},
-            {"id":4,"firstName":"Rodolfo","lastName":"Matos","email":"rodolfo@gmail.com","phone":"777333"}
-        ];
-        customerData2.forEach(function(element) {
+       
+        customerData.forEach(function(element) {
     
             elementStr =
                 "<td>" +
@@ -72,4 +58,13 @@ function populateCustomers() {
         row.setAttribute("id", "custumer-" + element.id);
         row.setAttribute("class", "customer-data");
     });
+        }
+        };
+
+        // start the AJAX request
+        ajax.open('GET', 'http://localhost:8080/api/customer', true);
+        ajax.setRequestHeader('Content-type', 'application/json');
+        ajax.send();
+      
+       
 }
